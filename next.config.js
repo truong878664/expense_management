18881fdc2f69684b9b514faab4fc45859a6f38b4
Trần Quốc Trường <110073086+truong878664@users.noreply.github.com>
2023-12-05 09:38:54 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  "output": "export",
+  "distDir": "dist",
   env: {
     TIME_ZONE: { timeZone: "Asia/Ho_Chi_Minh" },
     LOCAL_CODE: "vi-VN",
